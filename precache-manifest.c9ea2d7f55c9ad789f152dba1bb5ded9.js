2053d9c239ec146c73e7292139d6ec8bc5702eeb
LukasG85 <lukas85b@gmail.com>
2019-12-15 14:31:46 +0100

    Updates

---
+++ precache-manifest.c9ea2d7f55c9ad789f152dba1bb5ded9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3320db7ee37e8b83115f4430635b172",
+    "url": "/air-quality/index.html"
+  },
+  {
+    "revision": "c80d181a121e1327b5cf",
+    "url": "/air-quality/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f6abe4095388a66a2d00",
+    "url": "/air-quality/static/css/main.52efb984.chunk.css"
+  },
+  {
+    "revision": "c80d181a121e1327b5cf",
+    "url": "/air-quality/static/js/2.3d88c707.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/air-quality/static/js/2.3d88c707.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6abe4095388a66a2d00",
+    "url": "/air-quality/static/js/main.f1ac0529.chunk.js"
+  },
+  {
+    "revision": "3045d913ed4cb0b849e6",
+    "url": "/air-quality/static/js/runtime-main.e4a3f36f.js"
+  },
+  {
+    "revision": "8251da4aad18e6f37cd4e97101cb66ad",
+    "url": "/air-quality/static/media/bcg.8251da4a.jpg"
+  }
+]);
