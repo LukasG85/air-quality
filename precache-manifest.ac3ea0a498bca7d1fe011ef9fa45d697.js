8f1d11800ca24c8744a27ffdb58e7a0fb1692e2e
LukasG85 <lukas85b@gmail.com>
2019-12-15 21:34:27 +0100

    Updates

--- precache-manifest.ac3ea0a498bca7d1fe011ef9fa45d697.js
+++ precache-manifest.ac3ea0a498bca7d1fe011ef9fa45d697.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ec94ca1d4f13cc4632b323dc85384ded",
+    "revision": "d41415a9b97111d0080e6eea2ff62fe6",
     "url": "/air-quality/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/air-quality/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "f49768915a99ad92e2ae",
-    "url": "/air-quality/static/css/main.52efb984.chunk.css"
+    "revision": "d3f61684d78b0a12d902",
+    "url": "/air-quality/static/css/main.63309428.chunk.css"
   },
   {
     "revision": "2e506a329b33ee31c64b",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/air-quality/static/js/2.d924335d.chunk.js.LICENSE"
   },
   {
-    "revision": "f49768915a99ad92e2ae",
-    "url": "/air-quality/static/js/main.6a40796f.chunk.js"
+    "revision": "d3f61684d78b0a12d902",
+    "url": "/air-quality/static/js/main.62ed5bbd.chunk.js"
   },
   {
     "revision": "3045d913ed4cb0b849e6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/air-quality/precache-manifest.dc47aa121a3e2cb4b5632036c217587d.js"
+  "/air-quality/precache-manifest.ac3ea0a498bca7d1fe011ef9fa45d697.js"
 );
 
 self.addEventListener('message', (event) => {
